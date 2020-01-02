ac0c2538218374c68b86f947a5b7143b538f5181
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-01-01 19:13:05 -0600

    Added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBbx0WzS8Q4lS6fsBKVpOfLxeEFJvPmMJQ',
+    authDomain: 'ingreso-egreso-ac418.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-ac418.firebaseio.com',
+    projectId: 'ingreso-egreso-ac418',
+    storageBucket: 'ingreso-egreso-ac418.appspot.com',
+    messagingSenderId: '886394670107',
+    appId: `1:886394670107:web:ef0666de1d3f598273fcbe`
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBbx0WzS8Q4lS6fsBKVpOfLxeEFJvPmMJQ',
+    authDomain: 'ingreso-egreso-ac418.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-ac418.firebaseio.com',
+    projectId: 'ingreso-egreso-ac418',
+    storageBucket: 'ingreso-egreso-ac418.appspot.com',
+    messagingSenderId: '886394670107',
+    appId: `1:886394670107:web:ef0666de1d3f598273fcbe`
+  }
 };
 
 /*
