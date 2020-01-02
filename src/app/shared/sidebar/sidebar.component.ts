e58bd0a3b6c930ef1b19a584abc081a25cc1cb6d
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2020-01-01 19:13:50 -0600

    Added logout w/ @angular/fire

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  logout() {
+    console.log('%c [ LOGOUT - Component ] value', 'color: yellow' );
+    this.authService.logout();
   }
 
 }
